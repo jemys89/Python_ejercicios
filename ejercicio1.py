202089054278d1de9e4f3b1785d70c091cc6d270
jemys89 <jemys89@gmail.com>
2016-10-14 18:19:47 +0200

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+# ejercicio 1 de programación
+#Jamal y simar
+
+print("****Programa que te da el doble y el triple del número que pidas****")
+
+numero = int(input("Introduzca cualquier numero: "))
+
+
+print (numero)
+print (numero**2)
+print (numero**3)
---
+++ ejercicio2.py
@@ -0,0 +1,37 @@
+# ejercicio2  de programación
+#Jamal
+from math import pi
+def area_circunferencia 
+  return (pi*radio**2)
+
+def altura_circunferencia
+   return (radio**2)
+def calcular_altura
+    ("Permite calcular la altura de una circunferencia")
+
+if radio > 0:
+print ("****Pide el radio de una circunferencia****")
+else:
+    print ("error debes introducir un número superior a 0")
+
+#Programa principal
+
+quit= False
+
+while not quit:
+    print (" ")
+seleccion= raw_imput("¿Que quieres hacer, calcular área(1),altura(2) o salir(3)")
+radio = input ("Introduzca cualquier numero: ")
+
+
+if seleccion==('1'):
+    (calcular_area)
+elif seleccion==('2')
+   (calcular_altura)
+else:
+    quit= True
+    
+
+
+
+
