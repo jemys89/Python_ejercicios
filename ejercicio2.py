78296fe3fb771ef063fc46bc147e7abc6376df95
jemys89 <jemys89@gmail.com>
2016-10-16 21:56:43 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,9 +2,7 @@
 #Jamal
 from math import pi
 radio = float(input ( ' Teclee el radio de una circunferencia: ' ) )
-print ("Su longitud es")
-print (2 * pi * radio)
-print ("Su área es")
-print (pi * (radio**2))
+print ("Su longitud es",(2 * pi * radio))
+print ("Su area es",(pi * (radio**2)))
 
 
