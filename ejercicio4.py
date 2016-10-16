eaa5fa38d1a601d2747d94647eccd8f60181854d
jemys89 <jemys89@gmail.com>
2016-10-16 21:53:35 +0200

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,8 @@
+# ejercicio4  de programación
+#Jamal
+
+area=float (input('Teclee los metros de la base  de su finca:' ))
+altura=float(input('Teclee la altura de su finca:' ))
+print ('Los metros cuadrados de su finca son:',(area*altura),('metros cuadrados'))
+print ('El perímetro de su finca es',(area + area + altura + altura))
+
