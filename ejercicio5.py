e7f3b6989d02fdb14ee66895ea76845fa3bbfdb1
jemys89 <jemys89@gmail.com>
2016-10-16 22:04:42 +0200

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,10 @@
+# ejercicio5  de programación
+#Jamal
+sueldo=float(input('Teclee su sueldo bruto:' ))
+retencion= (sueldo * 0.20)
+print ('Su sueldo descontando la retención del 20% es:', (sueldo - retencion))
+
+
+
+
+
