1ea456142b5899486973aa6f0d25eaca68530b4a
jemys89 <jemys89@gmail.com>
2016-10-16 19:00:37 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,37 +1,10 @@
 # ejercicio2  de programación
 #Jamal
 from math import pi
-def area_circunferencia 
-  return (pi*radio**2)
-
-def altura_circunferencia
-   return (radio**2)
-def calcular_altura
-    ("Permite calcular la altura de una circunferencia")
-
-if radio > 0:
-print ("****Pide el radio de una circunferencia****")
-else:
-    print ("error debes introducir un número superior a 0")
-
-#Programa principal
-
-quit= False
-
-while not quit:
-    print (" ")
-seleccion= raw_imput("¿Que quieres hacer, calcular área(1),altura(2) o salir(3)")
-radio = input ("Introduzca cualquier numero: ")
-
-
-if seleccion==('1'):
-    (calcular_area)
-elif seleccion==('2')
-   (calcular_altura)
-else:
-    quit= True
-    
-
-
+radio = float(input ( ' Teclee el radio de una circunferencia: ' ) )
+print ("Su longitud es")
+print (2 * pi * radio)
+print ("Su area es")
+print (pi * (radio**2))
 
 
