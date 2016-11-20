cfdc93e88d26b6c4880a5ff3398f49968999b0d6
jemys89 <jemys89@gmail.com>
2016-11-20 22:51:51 +0100

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,10 +1,11 @@
-#ejercicio7 de programación
-#Jamal
+#Escribe un programa que pida por teclado un número y que a continuación muestre el mensaje el número leído
+#es positivo o bien el número leído es negativo dependiendo de que el número introducido por teclado sea positivo o negativo. 
+#Consideramos al número 0 positivo.
+#Jamal El Boutaibi
 
-
-numero= float(input("Teclee cualquier número:"))
-if(numero>=0):
+numero= int(input("Teclee cualquier número:"))
+if numero >= 0:
     print("Su número es positivo")
-elif(numero<0):
-    print("Su número es negativo")
+else:
+	print("Su número es negativo")
         
