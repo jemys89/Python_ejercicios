00351e5ab1a9d15b06405700e3329c8ddd13ef8b
jemys89 <jemys89@gmail.com>
2016-11-20 22:44:33 +0100

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,11 +1,14 @@
-#ejercicio6
+#Escribe un programa que pida por teclado dos valores de tipo numérico que se han de guardar
+#en sendas variables. ¿Qué instrucciones habría que utilizar para intercambiar su contenido? 
+#(es necesario utilizar una variable auxiliar). Para comprobar que el algoritmo ideado es correcto, 
+#muestra en pantalla el contenido de las variables una vez leídas, y vuelve a mostrar su contenido una
+# vez hayas intercambiado sus valores.
+
 #Jamal
 
-num1=float(input("teclee cualquier número: " ))
-num2=float(input("Teclee un número diferente al anterior: "))
-print ("Sus números elegidos son:",num1, num2)
-num3=num2
-print(num3)
-print(num1)
-print("Sus números han sido cambiados de variable")
+numeroUno=float(input("teclee cualquier número: " ))
+numeroDos=float(input("Teclee un número diferente al anterior: "))
+print ("Sus números elegidos son:",numeroUno, numeroDos)
+numeroAuxiliar= numeroDos
+print("Sus números han sido cambiados de preferencia ",numeroAuxiliar,numeroUno)
 
