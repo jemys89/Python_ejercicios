e4d61de2264d9f9cb9d467283b1367577d238fef
jemys89 <jemys89@gmail.com>
2016-11-20 22:01:25 +0100

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,8 +1,9 @@
-# ejercicio4  de programación
+#Escribe un programa que calcule el área de una finca rectangular
+# en metros cuadrados, así como su perímetro exterior, también en metros.
+#Ejercicio propuesto en programación
 #Jamal
 
-area=float (input('Teclee los metros de la base  de su finca:' ))
-altura=float(input('Teclee la altura de su finca:' ))
+area = float(input('Teclee los metros de la base  de su finca:' ))
+altura = float(input('Teclee la altura de su finca:' ))
 print ('Los metros cuadrados de su finca son:',(area*altura),('metros cuadrados'))
-print ('El perímetro de su finca es',(area + area + altura + altura))
-
+print ('El perímetro de su finca es',(area*2) + (altura*2))
