8ef1cbcc8a9b86fa9ec9cd948e3166d74d0aec8b
jemys89 <jemys89@gmail.com>
2016-11-21 12:43:58 +0100

    Add files via upload

---
+++ ejercicio15.py
@@ -0,0 +1,3 @@
+#Escribe un programa que escriba cinco veces el mensaje estamos estudiando metodología de la programación.
+for mensaje in range(5):
+	print("Estamos estudiando metodología de la programación.")
