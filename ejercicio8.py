d7c7a647e55572bcd911b42708fb3e84941b9ea2
jemys89 <jemys89@gmail.com>
2016-11-21 11:40:31 +0100

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,14 +1,17 @@
-#Ejercicio8 de programación
+#Escribe	un programa que pida por teclado dos números y que muestre en pantalla uno de los dos mensajes
+# siguientes en función de los números leídos: a) el primer número (valor) es mayor que el segundo (valor)(introduciendo
+# el valor de los números en el mensaje); b) el primer número (valor) es menor que el segundo (valor; c)
+# los dos números son iguales (valor).
+
 #Jamal
-#Ejercicio que se basa en la valoración de 2 números
 
 
-numero=float(input("teclee un número:" ))
-numero1=float(input("teclee el siguiente número a valorar:"))
-if(numero == numero1):
-    print(("sus números"),(numero),("y"),(numero1),("son iguales"))
-elif(numero > numero1):
-    print(("su primer número"),(numero),("es mayor que"),(numero1))
-elif(numero < numero1):
-    print(("su primer número"),(numero),("es menor que"),(numero1))
 
+numero = float(input("teclee un número:" ))
+numeroUno = float(input("teclee el siguiente número a valorar:"))
+if numero == numeroUno:
+    print(("sus números"),(numero),("y"),(numeroUno),("son iguales"))
+elif numero > numeroUno:
+    print(("su primer número"),(numero),("es mayor que"),(numeroUno))
+else:
+    print(("su primer número"),(numero),("es menor que"),(numeroUno))
