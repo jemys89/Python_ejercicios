92d0a9afab13514269d8eb9450b782f98db19af6
Jamal El Boutaibi Mohamed <jemys89@gmail.com>
2017-01-05 15:28:40 +0100

    Delete ejercicio16.py

--- ejercicio16.py
+++
@@ -1,8 +0,0 @@
-#Escribe un programa que escriba en pantalla los 30 primeros números naturales (del 1 al 30), así como su media aritmética.
-
-suma = 0
-for numero in range(31):
-	print (numero)
-	suma = suma + numero
-
-print ("La media aritmética del 1 al 30 es:",suma/30)
