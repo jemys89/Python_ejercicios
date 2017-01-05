8b04abf82c583168d5beadafdca520bc45881ab0
Jamal El Boutaibi Mohamed <jemys89@gmail.com>
2017-01-05 15:30:12 +0100

    Delete ejercicio5.py

--- ejercicio5.py
+++
@@ -1,9 +0,0 @@
-# Escribe un programa que calcule lo que tiene que cobrar un empleado sabiendo que se 
-#le tiene que aplicar al sueldo una retención del 20%.
-
-#Jamal
-sueldo = float(input('Teclee su sueldo bruto:' ))
-
-print ("Su sueldo descontando la retención es:", (sueldo * 0.8))
-
-
