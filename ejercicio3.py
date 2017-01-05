7b409617f17ccec5bb4b09ba6540430748f7ee80
Jamal El Boutaibi Mohamed <jemys89@gmail.com>
2017-01-05 15:30:25 +0100

    Delete ejercicio3.py

--- ejercicio3.py
+++
@@ -1,8 +0,0 @@
-#Repite el ejercicio 2 utilizando una constante que represente el valor de 	PI.
-#Ejercicio propuesto en clase de programación
-#Jamal
-#Lo he solucionado de la misma forma que en el ejercicio anterior.
-from math import pi
-radio = float(input ("Introduce el radio de una circunferencia: "))
-print ("Su longitud es",(2 * pi * radio))
-print ("Su area es",(pi * (radio**2)))
