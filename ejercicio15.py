bc47deb93aeb6c1ac279145ef96d9321679b315e
Jamal El Boutaibi Mohamed <jemys89@gmail.com>
2017-01-05 15:28:23 +0100

    Delete ejercicio15.py

--- ejercicio15.py
+++
@@ -1,3 +0,0 @@
-#Escribe un programa que escriba cinco veces el mensaje estamos estudiando metodología de la programación.
-for mensaje in range(5):
-	print("Estamos estudiando metodología de la programación.")
