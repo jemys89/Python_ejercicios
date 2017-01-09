d280efd48a5f31e195d3da29248a802d5606a3a1
Jamal El Boutaibi Mohamed <jemys89@gmail.com>
2017-01-09 23:16:52 +0100

    Add files via upload

---
+++ Vectores/ejercicio6Ajedrez.py
@@ -0,0 +1,19 @@
+#6. Crea un tablero de ajedrez (matriz 8 x 8). Los escaques de color negro se representan por un 1 y los
+#de color blanco con un 0. Muestra por pantalla el contenido de la matriz simulando un tablero de ajedrez.
+
+def ajedrez():
+	lineaCuadrado = [[1],[0]]*4
+	lineaCuadradoX = [[0],[1]]*4
+	print (str(lineaCuadrado))
+	print (str(lineaCuadradoX))
+	print (str(lineaCuadrado))
+	print (str(lineaCuadradoX))
+	print (str(lineaCuadrado))
+	print (str(lineaCuadradoX))
+	print (str(lineaCuadrado))
+	print (str(lineaCuadradoX))
+
+
+
+
+ajedrez()
