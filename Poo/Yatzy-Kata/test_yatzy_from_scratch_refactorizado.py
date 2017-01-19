c001e3f2d1105ca42f00922960653715d43dc8c7
jemys89 <jemys89@gmail.com>
2017-01-19 11:21:52 +0100

    Refactorizada la clase Yatzy

--- Poo/Yatzy-Kata/test_yatzy_from_scratch_refactorizado.py
+++ Poo/Yatzy-Kata/test_yatzy_from_scratch_refactorizado.py
@@ -1,6 +1,3 @@
-
-# No escribir con caracteres regionales
-
 import pytest
 from yatzy import Yatzy
 
@@ -38,25 +35,12 @@ def test_threes():
     assert 0 == Yatzy.threes(1, 1, 1, 1, 1)
     assert 9 == Yatzy.threes(3, 3, 3, 4, 5)
 
-# Los metodos fours, fives, sixes no son estaticos
-# Necesitamos un objeto de la clase Yatzy
-# Refactorizamos el constructor
-# Estructura de datos: una lista, ya que es mutable
-# y cada turno consta de 3 tiradas
-# Las tuplas no son mutables. Refactorizo los metodos
-# anteriores de tupla a lista <= no es necesario:
-# ya que son estaticos y no emplean un objeto Yatzy
-
 
 def test_constructor():
     tirada = Yatzy(1, 1, 1, 1, 1)
     for dado in tirada.dice:
         assert 1 == dado
 
-
-# La refactorizacion de los metodos fours, fives y sixes
-# consiste en simplificar los algoritmos para recorrer
-# la tirada de dados y sumar los puntos.
 
 @pytest.fixture
 def inyector():
@@ -68,8 +52,6 @@ def inyector():
 def test_fours(inyector):
     ### La suma de todos los CUATROS ###
     valorEsperado = 8
-    # No puedo testear con fixtures = inyeccion de dependencias
-    # los metodos estaticos como chance()
     assert valorEsperado == inyector.fours()
 
 
@@ -121,16 +103,10 @@ def test_four_of_a_kind():
     assert 8 == Yatzy.four_of_a_kind(2, 2, 2, 2, 2)
     assert 0 == Yatzy.four_of_a_kind(1, 2, 3, 4, 5)
 
-# Small straight:
-# When placed on "small straight", if the dice read
-#   1,2,3,4,5,
-# the player scores 15 (the sum of all the dice).
-#
-# El nombre del metodo no es consistente con la nomenclatura snake_case
-# El algoritmo es complicado e ineficiente.
 
 
 def test_small_straight():
+    ### Si la tirada de los dados es 1,2,3,4,5 signicará escalera pequeña y el jugador obtendrá 15 puntos ###
     assert 15 == Yatzy.small_straight(1, 2, 3, 4, 5)
     assert 0 == Yatzy.small_straight(2, 3, 4, 5, 6)
     assert 0 == Yatzy.small_straight(1, 3, 4, 5, 5)
@@ -138,15 +114,10 @@ def test_small_straight():
     assert 0 == Yatzy.small_straight(1, 2, 3, 4, 6)
 
 
-# Large straight:
-# When placed on "large straight", if the dice read
-#   2,3,4,5,6
-# the player scores 20 (the sum of all the dice).
-#
-# El nombre del metodo no es consistente con la nomenclatura snake_case
-# El algoritmo es complicado e ineficiente.
+
 
 def test_large_straight():
+    ### Si la tirada de los dados es 2,3,4,5,6 signicará escalera grande y el jugador obtendrá 20 puntos ###
     assert 20 == Yatzy.large_straight(2, 3, 4, 5, 6)
     assert 0 == Yatzy.large_straight(1, 2, 3, 4, 5)
     assert 0 == Yatzy.large_straight(1, 3, 4, 5, 5)
@@ -154,11 +125,10 @@ def test_large_straight():
     assert 0 == Yatzy.large_straight(1, 2, 3, 4, 6)
 
 
-# Full house:
-# If the dice are two of a kind and three of a kind, the
-# player scores the sum of all the dice.
 
-def test_fullHouse():
-    assert 8 == Yatzy.fullHouse(1, 1, 2, 2, 2)
-    assert 0 == Yatzy.fullHouse(2, 2, 3, 3, 4)
-    assert 0 == Yatzy.fullHouse(4, 4, 4, 4, 4)
+
+def test_full__house():
+    ### Si la tirada de los dados es de una pareja y un trío el jugador obtendrá la suma de todos sus dados ###
+    assert 8 == Yatzy.full_house(1, 1, 2, 2, 2)
+    assert 0 == Yatzy.full_house(2, 2, 3, 3, 4)
+    assert 0 == Yatzy.full_house(4, 4, 4, 4, 4)
--- Poo/Yatzy-Kata/yatzy.py
+++ Poo/Yatzy-Kata/yatzy.py
@@ -1,5 +1,8 @@
 class Yatzy:
 
+    def __init__(self, *dice):
+        self.dice = dice
+
     @staticmethod
     def chance(*dice):
         score = 0
@@ -28,13 +31,7 @@ class Yatzy:
         THREE = 3
         return dice.count(THREE) * THREE
 
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
+    
     
     def fours(self):
         FOUR = 4
@@ -84,81 +81,39 @@ class Yatzy:
         return 0
     
     @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
+    def four_of_a_kind(*dice):
+        FOUR = 4
+        for number in range(6,1,-1):
+            if dice.count(number) >= FOUR:
+                return number * FOUR
         return 0
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
-            return 15
-        return 0
+    def small_straight(*dice):
+        score = 15
+        for numero in range(1, 6):
+            if dice.count(numero) != 1:
+                return 0
+        return score
     
 
     @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
-            return 20
-        return 0
+    def large_straight(*dice):
+        score = 20
+        for numero in range(2, 7):
+            if dice.count(numero) != 1:
+                return 0
+        return score
     
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
-
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+    def full_house(*dice):
+        score = 0
+        for number in range(1, 7):
+            if dice.count(number) > 4 or dice.count(number) == 1:
+                return 0
+            elif dice.count(number) == 3:
+                score += number * 3
+            elif dice.count(number) == 2:
+                score += number * 2
+        return score
